c009c2ae250d5d4102bc4885b2d8a72626267b76
FILLOUX FLORIAN p2203403 <florian.filloux@etu.univ-lyon1.fr>
2023-07-18 16:17:03 +0200

    envoie email reussi et fini

--- BACK-END/public/style/js/contact.js
+++ BACK-END/public/style/js/contact.js
@@ -1,29 +1,34 @@
 /* RECUPERE LES INFORMATIONS DU FORMULAIRE ET LES ENVOIE A L'EXPERT */
-const nameInput = document.getElementById("Nom");
-const firstNameInput = document.getElementById("Préom"); 
+const lastNameInput = document.getElementById("Nom");
+const firstNameInput = document.getElementById("Prénom"); 
 const emailInput = document.getElementById("Email"); 
 const messageInput = document.getElementById("Message");
 const sendBtn = document.getElementById("submit");
+const form = document.getElementsByClassName("form")[0];
 
-//constante qui permet de recuperer toutes les valeurs des inputs et de les envoyer en email
-const recupMessage = () => {
-    //la valeur du Nom et du Prénom sont recupéré pour être les premières infos du mail
-    let valNameClient = nameInput.value;
-    let valFirstNameClient = firstNameInput.value;
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    console.log("clicked"); 
 
-    //la valeur du message est la partie la plus importante car c'est le contenu de la demande en mail
-    let valMessageClient = messageInput.value;    
+    Email.send({
+        SecureToken : "c571acaf-e336-47ea-af57-47b60db16d02",
+        To : 'forum.lyon1@gmail.com',
+        From : emailInput.value,
+        Subject : 'Message from ' + lastNameInput.value + ' ' + firstNameInput.value, 
+        Body : messageInput.value
+    }).then(
+      message => alert("L'email a bien était envoyé !")
+    );
 
-    //Pour conclure la valeur de l'Email nous permettra de pouvoir repondre à la personne le plus rapidement possible et on y fournit le notre aussi
-    let valEmailClient = emailInput.value;
-    let notreEmail = "leBOT_et_Juriste@gmail.com";
+    lastNameInput.value = '';
+    firstNameInput.value = '';
+    emailInput.value = '';
+    messageInput.value = '';
+});
 
-    /*
-        on envoie l'email avec pour syntaxe =
-            expéditeur : valEmailClient
-            destinataire : notreMail
-            Objet: valName valFirstName
-            Description : valMessage
-    */
-}
-sendBtn.addEventListener('click', recupMessage);
+    
+
+
+
+
+
